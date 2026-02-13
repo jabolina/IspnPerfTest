5d0075d88d671485471a69d15296f36ac18d8885
github-action-benchmark <github@users.noreply.github.com>
2026-02-13 23:51:21 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 8bb96ba54a995cda1ed3a3bab46f358776fa0d88

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771026681646,
+  "repoUrl": "https://github.com/jabolina/IspnPerfTest",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "committer": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "id": "8bb96ba54a995cda1ed3a3bab46f358776fa0d88",
+          "message": "Tracking benchmark results",
+          "timestamp": "2026-02-13T23:23:32Z",
+          "url": "https://github.com/jabolina/IspnPerfTest/commit/8bb96ba54a995cda1ed3a3bab46f358776fa0d88"
+        },
+        "date": 1771026680836,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Cluster Throughput",
+            "value": 206374.67,
+            "unit": "reqs/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
