3da4a551ab9ae9f614bb780e892dfa90f25684f4
github-action-benchmark <github@users.noreply.github.com>
2026-02-27 22:49:10 +0000

    add 8 nodes, dist-sync (Virtual Threads) (customBiggerIsBetter) benchmark result for 4f3d94f5ef4c597a9071ce54a23bd7b7e5812944

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772231688162,
+  "lastUpdate": 1772232550204,
   "repoUrl": "https://github.com/jabolina/IspnPerfTest",
   "entries": {
     "3n-ds-jdk25-vt-ispn16-jg5.5": [
@@ -86,6 +86,36 @@ window.BENCHMARK_DATA = {
             "value": 211248.83,
             "unit": "reqs/sec",
             "extra": "JDK 25, Infinispan 16.0.2, JGroups 5.5.3"
+          }
+        ]
+      }
+    ],
+    "8 nodes, dist-sync (Virtual Threads)": [
+      {
+        "commit": {
+          "author": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "committer": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "id": "4f3d94f5ef4c597a9071ce54a23bd7b7e5812944",
+          "message": "Trying dynamic tracking generation",
+          "timestamp": "2026-02-27T21:51:30Z",
+          "url": "https://github.com/jabolina/IspnPerfTest/commit/4f3d94f5ef4c597a9071ce54a23bd7b7e5812944"
+        },
+        "date": 1772232549827,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Cluster Throughput",
+            "value": 135833.99,
+            "unit": "reqs/sec",
+            "extra": "JDK 25, Infinispan 16.0.2, JGroups 5.5.1"
           }
         ]
       }
