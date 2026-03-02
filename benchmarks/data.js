5f3288ab54dc44c0fc23121fa2c46dc49702434b
José Augusto Bolina <jose@infinispan.org>
2026-03-02 11:35:56 -0300

    Delete benchmark entries for specific configuration
    Removed benchmark data for '3n-ds-jdk25-vt-ispn16-jg5.5'.

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -2,36 +2,6 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1772233567633,
   "repoUrl": "https://github.com/jabolina/IspnPerfTest",
   "entries": {
-    "3n-ds-jdk25-vt-ispn16-jg5.5": [
-      {
-        "commit": {
-          "author": {
-            "name": "José Bolina",
-            "username": "jabolina",
-            "email": "jose@infinispan.org"
-          },
-          "committer": {
-            "name": "José Bolina",
-            "username": "jabolina",
-            "email": "jose@infinispan.org"
-          },
-          "id": "8a106151a7641f1df61c491fd01220e4d7d6b1e6",
-          "message": "Trying dynamic tracking generation",
-          "timestamp": "2026-02-27T20:39:06Z",
-          "url": "https://github.com/jabolina/IspnPerfTest/commit/8a106151a7641f1df61c491fd01220e4d7d6b1e6"
-        },
-        "date": 1772226287324,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "Cluster Throughput",
-            "value": 203375.93,
-            "unit": "reqs/sec",
-            "extra": "nodes=3 config=dist-sync-gcp.xml jdk=25 vthreads=true"
-          }
-        ]
-      }
-    ],
     "3 nodes, dist-sync (Virtual Threads)": [
       {
         "commit": {
