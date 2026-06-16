457a8cca723c47bd33e75371fb803a5c87590608
github-action-benchmark <github@users.noreply.github.com>
2026-06-16 21:05:17 +0000

    add jabolina (customBiggerIsBetter) benchmark result for cfd0f597eeb53e98d544a571035c66f268e65b0e

---
+++ dev/jab/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1781643917711,
+  "repoUrl": "https://github.com/jabolina/IspnPerfTest",
+  "entries": {
+    "jabolina": [
+      {
+        "commit": {
+          "author": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "committer": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "id": "cfd0f597eeb53e98d544a571035c66f268e65b0e",
+          "message": "Adding extra configurations",
+          "timestamp": "2026-06-16T20:48:05Z",
+          "url": "https://github.com/jabolina/IspnPerfTest/commit/cfd0f597eeb53e98d544a571035c66f268e65b0e"
+        },
+        "date": 1781643916860,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Cluster Throughput",
+            "value": 77972.73,
+            "unit": "reqs/sec",
+            "extra": "Testing other configurations \nDefault Configuration (JDK: , ISPN: , JGroups: )"
+          }
+        ]
+      }
+    ]
+  }
+}
