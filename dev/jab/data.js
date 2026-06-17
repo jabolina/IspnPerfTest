25c2c366773b79d660a72482ec3fe0b5faab04d1
github-action-benchmark <github@users.noreply.github.com>
2026-06-17 16:14:23 +0000

    add jabolina (customBiggerIsBetter) benchmark result for 311b12d574cfadb0e9242c9e076d84b3932d5ebc

--- dev/jab/data.js
+++ dev/jab/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781643917711,
+  "lastUpdate": 1781712863374,
   "repoUrl": "https://github.com/jabolina/IspnPerfTest",
   "entries": {
     "jabolina": [
@@ -28,6 +28,34 @@ window.BENCHMARK_DATA = {
             "value": 77972.73,
             "unit": "reqs/sec",
             "extra": "Testing other configurations \nDefault Configuration (JDK: , ISPN: , JGroups: )"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "committer": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "id": "311b12d574cfadb0e9242c9e076d84b3932d5ebc",
+          "message": "Adding extra configurations",
+          "timestamp": "2026-06-17T15:59:37Z",
+          "url": "https://github.com/jabolina/IspnPerfTest/commit/311b12d574cfadb0e9242c9e076d84b3932d5ebc"
+        },
+        "date": 1781712863029,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Cluster Throughput",
+            "value": 86898.3,
+            "unit": "reqs/sec",
+            "extra": "Ugh \n8 nodes, dist-sync-tx, virtual threads (JDK: 25, ISPN: 16.2.0.Dev01, JGroups: 5.5.4)"
           }
         ]
       }
